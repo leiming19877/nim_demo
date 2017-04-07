3fc3220887a1154d1609a2bf64d2b62a2f00f296
leiming <leiming19877@163.com>
2017-04-07 15:54:54 +0800

    1、添加水印。

--- uikit/src/com/netease/nim/uikit/common/util/media/BitmapUtil.java
+++ uikit/src/com/netease/nim/uikit/common/util/media/BitmapUtil.java
@@ -3,9 +3,17 @@ package com.netease.nim.uikit.common.util.media;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Typeface;
+import android.text.Layout;
+import android.text.StaticLayout;
+import android.text.TextPaint;
 
 /**
  * Created by huangjun on 2016/9/19.
+ * @modify leiming
  */
 public class BitmapUtil {
 
@@ -71,5 +79,51 @@ public class BitmapUtil {
         }
 
         return inSampleSize;
+    }
+
+    /**
+     *  加水印
+     *  如果水印图片@watermark参数存在就加上水印图片
+     *  如果水印字符串@title 参数存在就加上水印字符串
+     * @param src  原图
+     * @param watermark 水印图片
+     * @param title 水印文字
+     * @return
+     */
+    public static Bitmap watermarkBitmap(Bitmap src, Bitmap watermark,
+                                         String title) {
+        if (src == null) {
+            return null;
+        }
+        int w = src.getWidth();
+        int h = src.getHeight();
+        //需要处理图片太大造成的内存超过的问题,这里我的图片很小所以不写相应代码了
+        Bitmap newb= Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);// 创建一个新的和SRC长度宽度一样的位图
+        Canvas cv = new Canvas(newb);
+        cv.drawBitmap(src, 0, 0, null);// 在 0，0坐标开始画入src
+        Paint paint = new Paint();
+        //加入图片
+        if (watermark != null) {
+            int ww = watermark.getWidth();
+            int wh = watermark.getHeight();
+            paint.setAlpha(50);
+            cv.drawBitmap(watermark, w - ww + 5, h - wh + 5, paint);// 在src的右下角画入水印
+        }
+        //加入文字
+        if(title!=null)
+        {
+            String familyName ="黑体";
+            Typeface font = Typeface.create(familyName,Typeface.NORMAL);
+            TextPaint textPaint=new TextPaint();
+            textPaint.setColor(Color.RED);
+            textPaint.setTypeface(font);
+            textPaint.setTextSize(100);
+            //这里是自动换行的
+            StaticLayout layout = new StaticLayout(title,textPaint,w, Layout.Alignment.ALIGN_NORMAL,1.0F,0.0F,true);
+            layout.draw(cv);
+        }
+        cv.save(Canvas.ALL_SAVE_FLAG);// 保存
+        cv.restore();// 存储
+        return newb;
     }
 }
