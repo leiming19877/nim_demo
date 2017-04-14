b5c38b04ec440a42db31d9824485d631235162c2
leiming <leiming19877@163.com>
2017-04-14 17:13:42 +0800

    1、测试代码。

--- app1/src/main/java/com/leiming/app1/MainActivity.java
+++ app1/src/main/java/com/leiming/app1/MainActivity.java
@@ -1,15 +1,28 @@
 package com.leiming.app1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.netease.nim.uikit.common.activity.UI;
 
 public class MainActivity extends UI {
 
+    private Button openPercent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        openPercent = (Button) this.findViewById(R.id.open_percent);
+        openPercent.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent  = new Intent();
+                intent.setClass(MainActivity.this,PercentActivity.class);
+                MainActivity.this.startActivity(intent);
+            }
+        });
     }
 }
---
+++ app1/src/main/java/com/leiming/app1/PercentActivity.java
@@ -0,0 +1,15 @@
+package com.leiming.app1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.netease.nim.uikit.common.activity.UI;
+
+public class PercentActivity extends UI {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_percent);
+    }
+}
